7c8834549ca3c05bf8d8d058099885469b9d1705
[Tharindu Balasooriya] <[tharinduudana66@gmail.com]>
2020-08-17 11:23:42 +0530

    finalize2

--- app/src/main/java/com/example/travelapp/MainActivity.java
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hotel_booking_preview);
+        setContentView(R.layout.activity_my_deals);
     }
 }
