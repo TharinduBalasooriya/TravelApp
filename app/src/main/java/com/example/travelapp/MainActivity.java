eed51c1288a64b40be699e20938e31dadd18b2ff
[Tharindu Balasooriya] <[tharinduudana66@gmail.com]>
2020-08-17 12:08:55 +0530

    finalize3

--- app/src/main/java/com/example/travelapp/MainActivity.java
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_deals);
+        setContentView(R.layout.activity_cardssummary);
     }
 }
--- app/src/main/java/com/example/travelapp/paymentsummary.java
+++ app/src/main/java/com/example/travelapp/paymentsummary.java
@@ -9,6 +9,6 @@ public class paymentsummary extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paymentsummary);
+        setContentView(R.layout.activity_cardssummary);
     }
 }
